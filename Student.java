65f877097fbe910d6101c0ea5076399550dfcc11
Jessie Zhang <47191695+JessieManUtd@users.noreply.github.com>
2019-01-30 11:41:02 -0800

    Assignment #3-StudentClass(Java)

---
+++ Student.java
@@ -0,0 +1,30 @@
+//Name: Wanhui Zhang(Jessie);
+//Student ID: 300293696;
+
+public class Student {
+    private String name;
+    private int score;
+
+    Student(String name, int score){
+        this.name = name;
+        this.score = score;
+    }
+    //setter methods
+    void setName(String Name) {
+        this.name = Name;
+    }
+    void setScore(int Score) {
+        this.score = Score;
+    }
+    //getter methods
+    public String getName() {
+        return name;
+    }
+    public int getScore() {
+        return score;
+    }
+
+    public String toString() {
+        return name + " - " + score;
+    }
+}
